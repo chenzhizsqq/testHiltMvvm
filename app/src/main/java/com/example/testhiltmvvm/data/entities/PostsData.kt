b7cd97fc46a4551f12b971223507d2405e23f974
chenzhi <chenzhi@qq.com>
2022-02-10 14:11:02 +0900

    数据的结构

---
+++ app/src/main/java/com/example/testhiltmvvm/data/entities/PostsData.kt
@@ -0,0 +1,5 @@
+package com.example.testhiltmvvm.data.entities
+
+data class PostsData(
+    val id: Int, val title: String
+)
---
+++ app/src/main/java/com/example/testhiltmvvm/data/entities/PostsDataList.kt
@@ -0,0 +1,5 @@
+package com.example.testhiltmvvm.data.entities
+
+data class PostsDataList(
+    val results: List<PostsData>
+)
