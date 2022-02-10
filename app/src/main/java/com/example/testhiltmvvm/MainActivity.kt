d82332f2441f6ecd4e8aaf465641ecd1a10aa0cc
chenzhi <chenzhi@qq.com>
2022-02-10 16:30:01 +0900

    示范一下在Hilt的Module，在系统中的作用。 就是自动创建，自动删除，自动封装的作用

--- app/src/main/java/com/example/testhiltmvvm/MainActivity.kt
+++ app/src/main/java/com/example/testhiltmvvm/MainActivity.kt
@@ -2,12 +2,60 @@ package com.example.testhiltmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.testhiltmvvm.data.remote.GithubJsonService
+import com.example.testhiltmvvm.data.remote.RemoteDataSource
+import com.example.testhiltmvvm.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var mRemoteDataSource : RemoteDataSource
+
+    @Inject
+    lateinit var mRetrofit : Retrofit
+
+    /**
+     *  在AppModule中的 provideGithubJsonService
+     *  自动让系统创建好了
+     */
+    @Inject
+    lateinit var mGithubJsonService : GithubJsonService
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        jsonGetAllGsonInject()
+    }
+
+
+    private fun jsonGetAllGsonInject() {
+        CoroutineScope(Dispatchers.IO ).launch {
+            val response = mGithubJsonService.getResponseGson()
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    try {
+                        binding.HelloWorld.text = response.body().toString()
+                        Log.e(TAG, "jsonGetAllGson: "+response.body() )
+                    } catch (e: Exception) {
+                        Log.e(TAG, "jsonGetPosts: ", e)
+                    }
+                } else {
+                    Log.e(TAG, "jsonGetPosts: error:" + response.message())
+                }
+            }
+        }
+    }
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
--- app/src/main/java/com/example/testhiltmvvm/di/AppModule.kt
+++ app/src/main/java/com/example/testhiltmvvm/di/AppModule.kt
@@ -14,16 +14,21 @@ import javax.inject.Singleton
 
 /**
  * 为整个APP创建 一个引用的Module
+ * @Provides就是专门为整个系统封装不用创建的对象
+ * 只要用@Inject来声名，就可以自动为你封装好
  */
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    /**
+     * 为整个APP创建 Gson 的 对象
+     */
     @Provides
     fun provideGson(): Gson = GsonBuilder().create()
 
     /**
-     * 为整个APP创建 Retrofit(gson: Gson)
+     * 为整个APP创建 Retrofit 的 对象
      */
     @Singleton
     @Provides
@@ -33,7 +38,7 @@ object AppModule {
         .build()
 
     /**
-     * 为整个APP创建 GithubJsonService(retrofit: Retrofit)
+     * 为整个APP创建 GithubJsonService 的 对象
      */
     @Provides
     fun provideGithubJsonService(retrofit: Retrofit): GithubJsonService = retrofit.create(
@@ -43,7 +48,7 @@ object AppModule {
 
 
     /**
-     * 为整个APP创建 RemoteDataSource(githubJsonService: GithubJsonService)
+     * 为整个APP创建 RemoteDataSource
      * 获取数据
      */
     @Singleton
